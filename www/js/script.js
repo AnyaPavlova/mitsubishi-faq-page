dbaaa2cbc0724b43359360f0ce29922641a9d0b1
Anya <likamoon@list.ru>
2020-09-21 17:38:41 +0300

    Initialization

---
+++ www/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(document).ready(function () {
+  /*Клик по ссылке-анкору */
+  $('.link-anchor').on("click", function (e) {
+    e.preventDefault();
+    var mylink = $(this).attr('href');
+    var positionblock = $(mylink).offset().top + 10; //вычисляем позицию блока
+
+    $('html, body').animate({
+      scrollTop: positionblock
+    }, 500);
+  });
+});
